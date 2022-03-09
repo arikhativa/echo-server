1363ef5e99247bad0170177f8cbaf2b55252d95f
Arik <arikhativa@gmail.com>
2022-03-09 17:02:07 +0200

    asd

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import echoHandler from "./handlers/echoHandler/echoHandler";
  
 const app = express()
 
+app.set("port", process.env.PORT || 8080);
 app.get('/', echoHandler.handler);
 
 export default app;
